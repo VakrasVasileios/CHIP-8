60ba0d81fd9646fcb9702fec909894691f89f36d
V.Vakras <vasilis_98@hotmail.com>
2020-09-04 02:00:50 +0300

    FetchFrom, SendTo implemented

---
+++ bus.cpp
@@ -0,0 +1,17 @@
+#include "bus.hpp"
+
+Bus::Bus() : memory(_memory) {}
+
+Bus::~Bus() {
+
+}
+
+uint8_t
+Bus::FetchFrom(uint16_t _address) {
+    return memory->LoadFrom(_address);
+}
+
+void
+Bus::SendTo(uint16_t _address, uint8_t _data) {
+    memory->StoreTo(_address, _data);
+}
