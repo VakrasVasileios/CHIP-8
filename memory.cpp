711248e35486b3bb075e66e6ad7f67724440466d
V.Vakras <vasilis_98@hotmail.com>
2020-09-04 01:34:26 +0300

    StoreTo, LoadFrom functions implemented

--- memory.cpp
+++ memory.cpp
@@ -10,15 +10,25 @@ Memory::~Memory() {
 
 uint8_t*
 Memory::GetProgram() {
-    return memory+(PROGRAM_LOCATION-1);
+    return &(memory+(PROGRAM_LOCATION-1));
 }
 
 uint8_t* 
 Memory::GetStack() {
-    return memory+stack_pointer+(STACK_LOCATION-1);
+    return &(memory+stack_pointer+(STACK_LOCATION-1));
 }
 
-uint8_t* 
+uint8_t*
 Memory::GetDisplayRefresh() {
-    return memory+(DISPLAY_REFRESH_LOCATION-1);
+    return &(memory+(DISPLAY_REFRESH_LOCATION-1));
+}
+
+void
+Memory::StoreTo(uint16_t _address, uint8_t _data) {
+    memory[_address] = _data;
+}
+
+uint8_t
+Memory::LoadFrom(uint16_t _address) {
+    return memory[_address];
 }
