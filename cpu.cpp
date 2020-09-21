d0a8a27124f8a678a1369c058164eef77f5dbe6a
V.Vakras <vasilis_98@hotmail.com>
2020-09-21 14:49:24 +0300

    12 cell stack, PopStack(), InsertToStack() added. Modified opcode funcs 0 to 3. 4 to 6, 19 and 29 opcode funcs added

--- cpu.cpp
+++ cpu.cpp
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include "cpu.hpp"
 
+uint16_t
+Cpu::PopStack() {
+    return stack[sp--];
+}
+
+void
+Cpu::InsertToStack(uint16_t _address) {
+    stack[++sp] = _address;
+}
+
 Cpu::Cpu(Memory* _memory, Display* _display) {
     bus = Bus(_memory, _display);
 
@@ -9,79 +19,96 @@ Cpu::Cpu(Memory* _memory, Display* _display) {
         bus->ClearDisplay();
     };
     // 00EE flow return
-    opcode[1] = [&, bus, sp, PC]() {
-        *PC = bus->FetchFrom(STACK(*sp));
-        *(sp--);
+    opcode[1] = [&PC]() {
+        *PC = PopStack();
     };
     // 1NNN flow goto NNN
-    opcode[2] = []() {
-
-    };
-    // 2NNN flow *(0x0NNN)()
-    opcode[3] = [&, sp, PC, bus](uint16_t _opcode) {
-        *sp++;
-        bus->SendTo(STACK(*sp), *PC);
+    opcode[2] = [&PC](uint16_t _opcode) {
+        InsertToStack(*PC);
         *PC = _opcode & 0x0fff;
     };
+    // 2NNN flow *(0x0NNN)()
+    opcode[3] = [&PC](uint16_t _opcode) {
+        InsertToStack(*PC);
+        *PC = _opcode & 0x0fff;
+    };
+    // 3XNN cond if(Vx == NN)
+    opcode[4] = [&PC](uint16_t _opcode) {
+        *PC += 4 * ((_opcode & 0x0f00) == (_opcode & 0x00ff));
+    };
+    // 4XNN cond if(Vx != NN)
+    opcode[5] = [&PC](uint16_t _opcode) {
+        *PC += 4 * ((_opcode & 0x0f00) != (_opcode & 0x00ff));
+    };
+    // 5XY0 cond if(Vx == Vy)
+    opcode[6] = [&PC](uint16_t _opcode) {
+        *PC += 4 * ((_opcode & 0x0f00) == (_opcode & 0x00f0))
+    }
     // 6XNN const Vx = NN
-    opcode[8] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = _opcode & 0x00ff;
+    opcode[7] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = _opcode & 0x00ff;
     };
     // 7XNN const Vx += NN
-    opcode[9] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] += _opcode & 0x0ff;
+    opcode[8] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] += _opcode & 0x0ff;
     };
     // 8XY0 assign Vx = Vy
-    opcode[10] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = registers[(_opcode & 0x00f0)];
+    opcode[9] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = v[(_opcode & 0x00f0)];
     };
     // 8XY1 bitop Vx = Vx | Vy
-    opcode[11] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = registers[(_opcode & 0x0f00)] | registers[(_opcode & 0x00f0)];
+    opcode[10] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = v[(_opcode & 0x0f00)] | v[(_opcode & 0x00f0)];
     };
     // 8XY2 bitop Vx = Vx & Vy
-    opcode[12] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = registers[(_opcode & 0x0f00)] & registers[(_opcode & 0x00f0)];
+    opcode[11] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = v[(_opcode & 0x0f00)] & v[(_opcode & 0x00f0)];
     };
     // 8XY3 bitop Vx = Vx ^ Vy
-    opcode[13] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = registers[(_opcode & 0x0f00)] ^ registers[(_opcode & 0x00f0)];
+    opcode[12] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = v[(_opcode & 0x0f00)] ^ v[(_opcode & 0x00f0)];
     };
     // 8XY4 math Vx += Vy
-    opcode[14] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] += registers[(_opcode & 0x00f0)];
+    opcode[13] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] += v[(_opcode & 0x00f0)];
     };
     // 8XY5 math Vx -= Vy
-    opcode[15] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] -= registers[(_opcode & 0x00f0)];
+    opcode[14] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] -= v[(_opcode & 0x00f0)];
     };
     // 8XY6 bitop Vx >>= Vy
-    opcode[16] = [&registers](uint16_t _opcode) {
-        registers[15] = registers[(_opcode & 0x0f00)] & 1;          // store least significant bit to VF
-        registers[(_opcode & 0x0f00)] >>= 1;
+    opcode[15] = [&v](uint16_t _opcode) {
+        v[15] = v[(_opcode & 0x0f00)] & 1;          // store least significant bit to VF
+        v[(_opcode & 0x0f00)] >>= 1;
     };
     // 8XY7  math Vx = Vy - Vx
-    opcode[17] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = registers[(_opcode & 0x00f0)] - registers[(_opcode & 0x0f00)];
+    opcode[16] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = v[(_opcode & 0x00f0)] - v[(_opcode & 0x0f00)];
     };
     // 8XYE bitop Vx <<= Vy
-    opcode[18] = [&registers](uint16_t _opcode) {
-        registers[15] = registers[(_opcode & 0x0f00)] & (1 << 7);   // store most significant bit to VF
-        registers[(_opcode & 0x0f00)] <<= 1;
+    opcode[17] = [&v](uint16_t _opcode) {
+        v[15] = v[(_opcode & 0x0f00)] & (1 << 7);   // store most significant bit to VF
+        v[(_opcode & 0x0f00)] <<= 1;
     };
     // 9XY0 cond if(Vx != Vy)
-
+    opcode[18] = [&PC](uint16_t _opcode) {
+        *PC += 4 * ((_opcode & 0x0f00) != (_opcode & 0x00f0));
+    };
     // ANNN mem I = NNN
-    opcode[20] = [&, registers, I](uint16_t _opcode) {
+    opcode[19] = [&, v, I](uint16_t _opcode) {
         I = _opcode & 0X0fff
     };
     // BNNN flow PC = V0 + NNN
-    opcode[21] = [&, registers, PC](uint16_t _opcode) {
-        PC = registers[0] + (_opcode & 0x0fff);
+    opcode[20] = [&, v, PC](uint16_t _opcode) {
+        PC = (_opcode & 0x0fff) + v[0];
     };
     // CXNN rand Vx = rand() & NN
-    opcode[22] = [&registers](uint16_t _opcode) {
-        registers[(_opcode & 0x0f00)] = (rand() % 256) & (_opcode & 0x00ff);
+    opcode[21] = [&v](uint16_t _opcode) {
+        v[(_opcode & 0x0f00)] = (rand() % 256) & (_opcode & 0x00ff);
+    };
+    // FX1E mem I += Vx
+    opcode[29] = [&I](uint16_t _opcode) {
+        *I += (_opcode & 0x0f00);
     };
 }
 
--- cpu.hpp
+++ cpu.hpp
@@ -4,17 +4,23 @@
 #include <functional>
 #include "bus.hpp"
 
+
+
 class Cpu {
 
 private:
     std::function<void(uint16_t)>* opcode[34];
 
-    uint8_t     registers[16];
+    uint8_t     v[16];              // 16 8bit registers
     uint16_t    I = 0;              // 16bit register for memory address
-    uint        PC = 0;             // program counter
-    uint        sp = 0;             // stack pointer index
+    uint16_t    PC = 0;             // program counter
+    uint        sp = 0;             // stack pointer (index)
+    uint16_t    stack[12];
 
     Bus         bus;
+
+    uint16_t    PopStack();
+    void        InsertToStack(uint16_t _address);
 
 public:
     Cpu(Memory* _memory, Display* _display);
